14d5eacb79eec16f4778acb78dbe1c008b77f845
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-02-10 23:47:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-AUTH_FILE="$HOME/.local/share/maxima/auth.toml"
-
-if [ ! -f "$AUTH_FILE" ]; then
-    mkdir -p "$(dirname "$AUTH_FILE")"
-    nano "$AUTH_FILE"
-fi
-
 xvfb-run -a \
     --server-args="-screen 0 1024x768x24" \
     env WINEPREFIX="$HOME/.local/share/maxima/wine/prefix" \
