acdc78ccca342110f95dfacda3fa64a50486924a
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-02-11 15:41:21 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-xvfb-run -a \
-    --server-args="-screen 0 1024x768x24" \
-    env WINEPREFIX="$HOME/.local/share/maxima/wine/prefix" \
-    "$HOME/ge-proton/files/bin/wine64" 123.exe || true
-
 xvfb-run -a --server-args="-screen 0 1024x768x24" maxima-cli
