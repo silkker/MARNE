ec97801ceee31e2f10f156580ea91b075d5b5573
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-02-13 14:29:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #tmux new-session -A -s maxima \
-#  'xvfb-run -a --server-args="-screen 0 1024x768x16" maxima-cli'
+#  'xvfb-run -a --server-args="-screen 0 1024x768x24" maxima-cli'
 
 # One day
 # 'xwayland-run -- maxima-cli'
