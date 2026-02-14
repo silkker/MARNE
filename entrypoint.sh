7dd9b0fe6ec327cb1ec5aaca72462cf54ab8288b
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-02-15 02:10:06 +0300

    Update and rename entrypoint.sh to maxima.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#tmux new-session -A -s maxima \
-#  'xvfb-run -a --server-args="-screen 0 1024x768x24" maxima-cli'
-
-# One day
-# 'xwayland-run -- maxima-cli'
-# 'wlheadless-run -c weston -- maxima-cli'
