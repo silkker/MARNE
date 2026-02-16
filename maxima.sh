ede3126c774b668a316773a1a597a16423a0db7e
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-02-16 23:09:52 +0300

    containerfile stuff

---
+++ maxima.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Run with `wlheadless-run -c cage -- $HOME/maxima.sh`
+# Do not add a TAB to disable EAAC installation it seems to cause issues.
+
+maxima-cli &
+(
+  sleep 30
+  while true; do
+    wtype -k Return
+    sleep 1
+  done
+)
